ee8ceae46f76d172b8e5d3da811ba5d373c64970
Subhash Vadadoriya <it.subhashpatel@gmail.com>
2018-12-08 13:49:54 +0530

    solve duplicate folder issue

--- install-tomcat-ubuntu.sh
+++ install-tomcat-ubuntu.sh
@@ -17,21 +17,17 @@ apt-get install unzip -y
 if java -version | grep -q "java version" ; then
   echo "Java Installed"
 else
-  sudo add-apt-repository ppa:webupd8team/java -y  && sudo apt-get update -y  && sudo apt-get install oracle-java8-installer -y && echo JAVA_HOME=/usr/lib/jvm/java-8-oracle >> /etc/environment && echo JRE_HOME=/usr/lib/jvm/java-8-oracle/jre >> /etc/environment && source /etc/environment
+  sudo add-apt-repository ppa:webupd8team/java -y  && sudo apt-get update -y  && echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections && sudo apt-get install oracle-java8-installer -y && echo JAVA_HOME=/usr/lib/jvm/java-8-oracle >> /etc/environment && echo JRE_HOME=/usr/lib/jvm/java-8-oracle/jre >> /etc/environment && source /etc/environment
 fi
-
-# Create Tomcat User
-useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
 
 # Install Tomcat
 cd /opt/
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.zip
 unzip apache-tomcat-9.0.8.zip
-mv apache-tomcat-9.0.8 tomcat
 
 # Set Permission for execute
-chown -RH tomcat: /opt/tomcat
-chmod o+x /opt/tomcat/bin/*.sh
+chown -RH tomcat: /opt/apache-tomcat-9.0.8
+chmod o+x /opt/apache-tomcat-9.0.8/bin/*.sh
 
 # Adjust the Firewall
 ufw allow 8080/tcp
@@ -50,13 +46,13 @@ echo  >> /etc/systemd/system/tomcat.service
 echo Environment="JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> /etc/systemd/system/tomcat.service
 echo Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom -Djava.awt.headless=true" >> /etc/systemd/system/tomcat.service
 echo  >> /etc/systemd/system/tomcat.service
-echo Environment="CATALINA_BASE=/opt/tomcat" >> /etc/systemd/system/tomcat.service
-echo Environment="CATALINA_HOME=/opt/tomcat" >> /etc/systemd/system/tomcat.service
-echo Environment="CATALINA_PID=/opt/tomcat/temp/tomcat.pid" >> /etc/systemd/system/tomcat.service
+echo Environment="CATALINA_BASE=/opt/apache-tomcat-9.0.8" >> /etc/systemd/system/tomcat.service
+echo Environment="CATALINA_HOME=/opt/apache-tomcat-9.0.8" >> /etc/systemd/system/tomcat.service
+echo Environment="CATALINA_PID=/opt/apache-tomcat-9.0.8/temp/tomcat.pid" >> /etc/systemd/system/tomcat.service
 echo Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC" >> /etc/systemd/system/tomcat.service
 echo  >> /etc/systemd/system/tomcat.service
-echo ExecStart=/opt/tomcat/bin/startup.sh >> /etc/systemd/system/tomcat.service
-echo ExecStop=/opt/tomcat/bin/shutdown.sh >> /etc/systemd/system/tomcat.service
+echo ExecStart=/opt/apache-tomcat-9.0.8/bin/startup.sh >> /etc/systemd/system/tomcat.service
+echo ExecStop=/opt/apache-tomcat-9.0.8/bin/shutdown.sh >> /etc/systemd/system/tomcat.service
 echo  >> /etc/systemd/system/tomcat.service
 echo [Install] >> /etc/systemd/system/tomcat.service
 echo WantedBy=multi-user.target >> /etc/systemd/system/tomcat.service
@@ -73,5 +69,5 @@ sudo systemctl enable tomcat
 rm apache-tomcat-9.0.8.zip
 apt-get autoremove
 
-echo Tomcat is successfully installed at /opt/tomcat
+echo Tomcat is successfully installed at /opt/apache-tomcat-9.0.8
 echo "you can start and stop tomcat using command : sudo service tomcat stop|start|status|restart" 
